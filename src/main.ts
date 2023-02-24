5b41f560f359cbc7e24705fca95aaac80259088f
AnCoSONG <1197252996@qq.com>
2023-02-25 04:04:38 +0800

    fix: 🐛 add polyfill

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './style.scss'
 import "normalize.css";
 import App from './App.vue'
 import router from './routers'
+import Promise from 'es6-promise';
+
+Promise.polyfill()
 
 const app = createApp(App)
 app.use(router)
